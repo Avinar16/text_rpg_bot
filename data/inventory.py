a485bd72a312baee9b5499a15e2ac27e89ba2ea1
PROlodki <72821917+PROlodki@users.noreply.github.com>
2021-04-29 00:26:10 +0300

    Update inventory.py

--- data/inventory.py
+++ data/inventory.py
@@ -8,10 +8,10 @@ class Inventory(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'inventory'
     # что с чем связываем - character.id с
     char_id = sqlalchemy.Column(sqlalchemy.Integer,
-                                sqlalchemy.ForeignKey('character.id'), primary_key=True)
+                                sqlalchemy.ForeignKey('character.id'))
     # items.id
     item_id = sqlalchemy.Column(sqlalchemy.Integer,
-                                sqlalchemy.ForeignKey('items.id'), primary_key=True)
+                                sqlalchemy.ForeignKey('items.id'))
 
     items = orm.relationship("Items", backref="characters")
     character = orm.relationship("Character", backref="inventory")
