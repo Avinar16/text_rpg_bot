6837e5c6f7d24ab053820fad2ec04a71c5ef9f94
PROlodki <72821917+PROlodki@users.noreply.github.com>
2021-04-29 01:38:57 +0300

    XotFix

--- functions/to_default_func/char_defaut.py
+++ functions/to_default_func/char_defaut.py
@@ -12,7 +12,7 @@ def char_default(update, context):
     db_sess = db_session.create_session()
     current_char = db_sess.query(Character).filter(Character.user_id == update.effective_user.id).first()
     inventory = db_sess.query(Inventory).filter(Inventory.char_id == current_char.id).all()
-    db_sess.delete(current_char)
     for item in inventory:
         db_sess.delete(item)
+    db_sess.delete(current_char)
     db_sess.commit()
